27fa0c5294878a41f299cbd4817d0955803b1dfe
sumeet978 <searchsumeet@gmail.com>
2019-09-24 14:59:27 +0100

    Refactored the code

--- lib/report.rb
+++ lib/report.rb
@@ -1,12 +1,10 @@
 class Report
   def class_result(input)
-  if (input == "Green")
-    return ("Green:1")
-  elsif (input == "Amber")
-    return ("Amber:1")
-  elsif (input == "Green,Green")
-    green_count = input.split(",").count("Green")
+    if (input == "Amber")
+      return ("Amber:1")
+    else (input == "Green,Green")
+      green_count = input.split(",").count("Green")
     return ("Green:#{green_count}")
-  end
+    end
   end
 end 
